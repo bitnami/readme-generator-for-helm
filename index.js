aa25811aa5311542ce000ff8f9cf641e43d873aa
Miguel A. Cabrera Minagorri <macabrera@bitnami.com>
2021-02-24 16:26:04 +0000

    Add CLI capabilities

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const { program } = require('commander');
 const { createValuesObject, parseMetadataComments } = require('./lib/parser');
 const { checkKeys } = require('./lib/checker');
