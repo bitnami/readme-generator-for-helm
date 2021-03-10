bcfb93454b24750bce6584aea792ab2cb6e47aee
Miguel A. Cabrera Minagorri <macabrera@bitnami.com>
2021-03-10 09:51:36 +0000

    Separate bin directory from default index exporting the sections

--- index.js
+++ index.js
@@ -6,38 +6,25 @@ const { checkKeys } = require('./lib/checker');
 const { buildSections } = require('./lib/builder');
 const { insertReadmeTable } = require('./lib/render');
 
-/** DOCS:
- * The comments structure for a parameter is "## @param (FullKeyPath)[Modifier?] Description"
- * The comments structure for a section is "## @section Section Title"
- * Modifiers: they allow to force a value for a parameter.
- * Modifiers supported: [array] Indicates the key is for an array to set the description as '[]'.
- *                      [object] Indicates the key is for an object to set the description as '[]'.
- * The only not supported case is when we add an array with actual values. Test what happens, maybe it prints the array values
- */
+function getValuesSections(options) {
+  const valuesFilePath = options.values;
+  const readmeFilePath = options.readme;
+  const configPath = options.config ? options.config : `${__dirname}/config.json`;
+  const CONFIG = require(configPath);
 
-program
-  .requiredOption('-r, --readme <path>', 'Path to the README.md file')
-  .requiredOption('-v, --values <path>', 'Path to the values.yaml file')
-  .option('-c, --config <path>', 'Path to the config file');
+  const valuesObject = createValuesObject(valuesFilePath);
+  let valuesMetadata = parseMetadataComments(valuesFilePath, CONFIG);
 
-program.parse(process.argv);
+  // Check the parsed keys are consistent with the real ones
+  checkKeys(valuesObject, valuesMetadata);
 
-const options = program.opts();
-const valuesFilePath = options.values;
-const readmeFilePath = options.readme;
-const configPath = options.config ? options.config : `${__dirname}/config.json`;
-const CONFIG = require(configPath);
+  // We don't need the skip objects anymore so filter them
+  valuesMetadata = valuesMetadata.filter((el) => el.skip ? false : true);
 
-const valuesObject = createValuesObject(valuesFilePath);
-let valuesMetadata = parseMetadataComments(valuesFilePath, CONFIG);
+  // Return sections array combining metadata and real values
+  return  buildSections(valuesObject, valuesMetadata);
+}
 
-// Check the parsed keys are consistent with the real ones
-checkKeys(valuesObject, valuesMetadata);
-
-// We don't need the skip objects anymore so filter them
-valuesMetadata = valuesMetadata.filter((el) => el.skip ? false : true);
-
-// Build sections array combining metadata and real values
-const sections = buildSections(valuesObject, valuesMetadata);
-
-insertReadmeTable(readmeFilePath, sections, CONFIG);
+module.exports = {
+  getValuesSections,
+}
