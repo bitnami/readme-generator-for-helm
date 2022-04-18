311262c743a34fb75843d3828439a45bca5b3585
Miguel A. Cabrera Minagorri <mcabrera@vmware.com>
2022-04-18 10:25:08 +0000

    Use JSON.parse instead of require and lower capital variables
    Signed-off-by: Miguel A. Cabrera Minagorri <mcabrera@vmware.com>

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 
+const fs = require('fs');
+
 const { createValuesObject, parseMetadataComments } = require('./lib/parser');
 const { checkKeys } = require('./lib/checker');
 const { combineMetadataAndValues, buildSectionsArrays, buildParamsToRenderList } = require('./lib/builder');
@@ -14,10 +16,10 @@ const { insertReadmeTable, renderOpenAPISchema } = require('./lib/render');
 function getParameters(options) {
   const valuesFilePath = options.values;
   const configPath = options.config ? options.config : `${__dirname}/config.json`;
-  const CONFIG = require(configPath);
+  const config = require(configPath);
 
   const valuesObject = createValuesObject(valuesFilePath);
-  const valuesMetadata = parseMetadataComments(valuesFilePath, CONFIG);
+  const valuesMetadata = parseMetadataComments(valuesFilePath, config);
 
   // Check the parsed keys are consistent with the real ones
   checkKeys(valuesObject, valuesMetadata);
@@ -36,26 +38,26 @@ function runReadmeGenerator(options) {
   const versionFlag = options.version;
 
   if (versionFlag) {
-    console.log("Version:", require('./package.json').version);
+    console.log("Version:", JSON.parse(fs.readFileSync('./package.json')).version);
   } else {
     if (!readmeFilePath && !schemaFilePath) {
-      throw new Error('Nothing to do. Please provide a README file or Schema file output.');
+      throw new Error('Nothing to do. Please provide the --readme or --schema options.');
     }
     if (!valuesFilePath) {
-      throw new Error('Values file not provided');
+      throw new Error('Nothing to do. You must provide the --value option');
     }
     const configPath = options.config ? options.config : `${__dirname}/config.json`;
-    const CONFIG = require(configPath);
+    const config = JSON.parse(fs.readFileSync(configPath));
     const parametersList = getParameters(options);
 
     if (readmeFilePath) {
-      const paramsToRender = buildParamsToRenderList(parametersList, CONFIG);
+      const paramsToRender = buildParamsToRenderList(parametersList, config);
       const sections = buildSectionsArrays(paramsToRender);
-      insertReadmeTable(readmeFilePath, sections, CONFIG);
+      insertReadmeTable(readmeFilePath, sections, config);
     }
 
     if (schemaFilePath) {
-      renderOpenAPISchema(schemaFilePath, parametersList, CONFIG);
+      renderOpenAPISchema(schemaFilePath, parametersList, config);
     }
   }
 }
